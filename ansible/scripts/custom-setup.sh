d5f3809e6e54bc36a59c5e1f167275ea894230d2
Volker Eyrich <volkere@google.com>
2022-09-20 22:30:39 +0000

    remove GCS mounting commands from custom setup; now handled via /etc/fstab

--- ansible/scripts/custom-setup.sh
+++ ansible/scripts/custom-setup.sh
@@ -2,23 +2,5 @@
 
 #any one-shot custom setup required at startup
 
-#GCS buckets can also be mounted via the usual /etc/fstab mechanism, i.e.
-#
-# [bucket-name]        /bucket-mount-point gcsfuse rw,user,dir_mode
-#
-# details on options at: https://github.com/GoogleCloudPlatform/gcsfuse/blob/master/docs/mounting.md
-
-if [ ! -d /gcs/bucket-one ]
-then
-   mkdir -p /gcs/bucket-one
-fi
-gcsfuse -o allow_other -file-mode=777 -dir-mode=777 --implicit-dirs [bucket_one] /gcs/bucket-one
-
-if [ ! -d /gcs/bucket-two ]
-then
-   mkdir -p /gcs/bucket-two
-fi
-gcsfuse -o allow_other -file-mode=777 -dir-mode=777 --implicit-dirs [bucket_two] /gcs/bucket-two
-
 #record completion
 date > /tmp/custom-setup-finished
